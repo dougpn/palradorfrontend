41745dd941fe2e23b304e7ffa72c9cb6bc4ecba4
dougpn <douglas@cotabox.com.br>
2021-11-25 09:12:25 -0300

    fix: Correção na identação

--- src/pages/Postedit/styles.js
+++ src/pages/Postedit/styles.js
@@ -1,48 +1,48 @@
 import { StyleSheet, StatusBar } from 'react-native';
 
 export default StyleSheet.create({
-    container:{
-        flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: 50
-    },
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 50
+  },
 
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
 
-    headerText: {
-        fontSize: 15,
-        color: '#737380'
-    },
+  headerText: {
+    fontSize: 15,
+    color: '#737380'
+  },
 
-    headerTextBold: {
-        fontWeight: 'bold'
-    },
-    container2: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
+  headerTextBold: {
+    fontWeight: 'bold'
+  },
+  container2: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
 
-    actionpostButton: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
+  actionpostButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
 
-    actionpostButton2: {
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: '10%'
-    },
+  actionpostButton2: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '10%'
+  },
 
 
-    actionpostButtonText: {
-        color:'#e02041',
-        fontSize: 15,
-        fontWeight: 'bold'
-    },
+  actionpostButtonText: {
+    color: '#e02041',
+    fontSize: 15,
+    fontWeight: 'bold'
+  },
 })
