07f18f1881a1489d56bea1774252fe6de90a294d
dougpn <douglas@cotabox.com.br>
2021-11-30 00:20:23 -0300

    feature: Adicionada lógica de rotas proibidas usando redux

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { connect } from 'react-redux'
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import store from './store';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Homeadm from './pages/Homeadm';
 import Postcreate from './pages/Postcreate';
 import Postedit from './pages/Postedit';
+import SplashScreen from './pages/SplashScreen';
 
 const Stack = createStackNavigator();
 
-export default function Routes() {
+
+function Routes() {
+  const state = store.getState()
+  if (state.isLoading) {
+    return <SplashScreen />;
+  }
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false}} initialRouteName="Login">
-        <Stack.Screen name="Login" component={ Login } />
-        <Stack.Screen name="Register" component={ Register } />
-        <Stack.Screen name="Homeadm" component={ Homeadm } />
-        <Stack.Screen name="Postcreate" component={ Postcreate } />
-        <Stack.Screen name="Postedit" component={ Postedit } />
+      <Stack.Navigator screenOptions={{ headerShown: false}}>
+        { state.userToken == null ? (
+          <>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Homeadm" component={Homeadm} />
+            <Stack.Screen name="Postcreate" component={Postcreate} />
+            <Stack.Screen name="Postedit" component={Postedit} />
+          </>
+        ) }
       </Stack.Navigator>
     </NavigationContainer>
   )
-};
+}
+export default connect((state) => state)(Routes);
